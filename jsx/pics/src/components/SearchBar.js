05f9f543826c2a9de2b4390428c542fd45b82b2b
popo26 <mynameispopo26@gmail.com>
2023-08-14 14:43:00 +1200

    parent child data flow

---
+++ jsx/pics/src/components/SearchBar.js
@@ -0,0 +1,16 @@
+function SearchBar({onSubmit}){
+const handleFormSubmit = (event) =>{
+    event.preventDefault();
+    onSubmit('cars');
+}
+
+    return <div>
+        <form onSubmit={handleFormSubmit}>
+        <input/>
+        </form>
+       
+       
+    </div>
+}
+
+export default SearchBar;
