fc0cc9ad9bdbfec2a87130877a94c0cf886bd6e0
popo26 <mynameispopo26@gmail.com>
2023-08-18 10:35:16 +1200

    books upgraded with json-server

--- jsx/books/src/App.js
+++ jsx/books/src/App.js
@@ -1,21 +1,41 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from "axios";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+
 
 function App() {
     const [books, setBooks] = useState([]);
 
-    const editBookById = (id, newTitle) => {
+    const fetchBooks = async () => {
+        const response = await axios.get("http://localhost:3001/books");
+
+        setBooks(response.data);
+    };
+
+    useEffect(() => {
+        fetchBooks();
+    }, []);
+
+    const editBookById = async (id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle
+        });
+
+        console.log(response);
+
         const updatedBooks = books.map((book) => {
             if (book.id === id) {
-                return {...book, title:newTitle};
+                return { ...book, ...response.data };
             }
             return book;
         });
         setBooks(updatedBooks);
     };
 
-    const deleteBookById = (id) => {
+    const deleteBookById = async (id) => {
+
+        await axios.delete(`http://localhost:3001/books/${id}`);
         const updatedBooks = books.filter((book) => {
             return book.id !== id;
         });
@@ -23,13 +43,18 @@ function App() {
         setBooks(updatedBooks);
     };
 
-    const createBook = (title) => {
+    const createBook = async (title) => {
+
+        const response = await axios.post("http://localhost:3001/books", {
+            title
+        });
+
+        console.log(response);
+
+
         const updatedBooks = [
             ...books,
-            {
-                id: Math.round(Math.random() * 9999),
-                title: title
-            }
+            response.data
         ];
         setBooks(updatedBooks);
     };
@@ -39,7 +64,7 @@ function App() {
         <div className="app">
             <h1>Reading List</h1>
             <BookList
-            onEdit={editBookById}
+                onEdit={editBookById}
                 books={books}
                 onDelete={deleteBookById}
             />
